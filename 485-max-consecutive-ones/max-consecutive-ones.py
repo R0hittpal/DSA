a547b7f6828909fbfb0e5e09c20e535b0abbf115
Rohit Kumar Pal😶‍🌫️🤩 <rohitpal0948@gmail.com>
2026-09-06 21:22:15 +0530

    Time: 16 ms (53.91%) | Memory: 21.9 MB (43.60%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -5,7 +5,7 @@ class Solution:
         for i in nums:
             if i ==1:
                 count+=1
-            # max_count=max(count,max_count)
+            
             else:
                 count=0   
             max_count=max(count,max_count)

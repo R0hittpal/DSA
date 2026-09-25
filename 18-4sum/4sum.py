c2fc6c200f59c297909dfa6d37a3002309f683cd
Rohit Kumar Pal😶‍🌫️🤩 <rohitpal0948@gmail.com>
2026-09-25 07:04:45 +0530

    Time: 379 ms (78.36%) | Memory: 19.5 MB (8.32%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,16 +1,32 @@
 class Solution:
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
-        ans=set()
+        ans=[]
         n=len(nums)
-        for i in range(n-2):
-            
-            for j in range(i+1,n-1):
-                seen=set()
-                for k in range(j+1,n):
-                    composite=target-(nums[i]+nums[j]+nums[k])
-                    if composite in seen:
-                        ans.add(tuple(sorted([nums[i],nums[j],nums[k],composite])))
-                    seen.add(nums[k])
+        nums.sort()
+        for i in range(n-3):
+            if i >0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,n-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                
+                left=j+1
+                right=n-1
+                while left < right:
+                    total=nums[i]+nums[j]+nums[left]+nums[right]
+
+                    if total == target:
+                        ans.append([nums[i],nums[j],nums[left],nums[right]])
+                        right-=1
+                        left+=1
+                        while left<right and nums[left]==nums[left-1]:
+                            left+=1
+                        while left<right and nums[right]==nums[right+1]:
+                            right-=1
+                    elif total <target:
+                        left+=1
+                    else:
+                        right-=1
         return [list(x) for x in ans]
 
-        
+

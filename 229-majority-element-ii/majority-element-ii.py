0c29f88f0f09fe054b8fc1b062041e144a8467ac
Rohit Kumar Pal😶‍🌫️🤩 <rohitpal0948@gmail.com>
2026-09-25 09:57:19 +0530

    Time: 11 ms (34.98%) | Memory: 22.2 MB (92.15%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,49 +1,49 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> list[int]:
         
-    
+        element1=None
+        count1=0
+        element2=None
+        count2=0
+        n=len(nums)
 
-        element1 = 0
-        count1 = 0
-        element2 = 0
-        count2 = 0
+        # if all(x == 0 for x in nums):
+        #     return [0]
 
-        for num in nums:
 
-            if count1 == 0 and num != element2:
-                element1 = num
-                count1 = 1
+        for i in range(n):
+            if count1==0 and nums[i]!=element2:
+                element1 =nums[i]
+                count1=1
+            elif count2==0 and nums[i]!=element1:
+                element2 =nums[i]
+                count2=1
 
-            elif count2 == 0 and num != element1:
-                element2 = num
-                count2 = 1
-
-            elif num == element1:
-                count1 += 1
-
-            elif num == element2:
-                count2 += 1
-
+            elif nums[i]==element1:
+                count1+=1
+            elif nums[i]==element2:
+                count2+=1
             else:
-                count1 -= 1
-                count2 -= 1
-
-        # Verification
-        count1 = 0
-        count2 = 0
-
-        for num in nums:
-            if num == element1:
-                count1 += 1
-            elif num == element2:
-                count2 += 1
-
-        ans = []
-
-        if count1 > len(nums) // 3:
+                count1-=1
+                count2-=1
+        ans=[]           
+        count3=0
+        for i in range(n):
+            if nums[i]==element1:
+                count3+=1
+        if count3>(n//3):
             ans.append(element1)
 
-        if count2 > len(nums) // 3:
+        count4=0
+        for i in range(n):
+            if nums[i]==element2:
+                count4+=1
+        if count4>(n//3):
             ans.append(element2)
-
         return ans
+
+    
+        
+            
+
+        
